13fc70aff09dc345783e85b492881284451e2aa9
Gustavo Willer <gustavo.willer@hotmail.com>
2018-09-24 22:45:46 -0300

    Include model, controller, and migration of Address

---
+++ database/migrations/2018_09_25_013553_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('main');
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement');
+            $table->string('neighborhood');
+            $table->string('cep');
+            $table->integer('city_id');
+            $table->integer('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
