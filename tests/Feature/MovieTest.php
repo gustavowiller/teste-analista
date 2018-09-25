6bd1cf4a3b170d6000068bfd6a4f5d540c2f7c78
Gustavo Willer <gustavo.willer@hotmail.com>
2018-09-25 14:05:22 -0300

    Include test Movie

---
+++ tests/Feature/MovieTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MovieTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    /** @test */
+    public function a_user_can_browse_movies()
+    {
+        $response = $this->get('/movies');
+
+        $response->assertStatus(200);
+    }
+}
